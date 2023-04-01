5cc03be1a015f912456b7296b7c5796f4c8b6805
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-04-02 00:55:25 +0530

    ft:home section done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60171bc9dc4ae96c"; }
+/******/ 		__webpack_require__.h = function() { return "a192c1ae65b94562"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
