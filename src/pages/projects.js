7c17c8369aff793eb18e136cd3424fa196386bff
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-04-03 13:14:54 +0530

    ft:carousel added

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import Carousel from '@/components/Carousel'
+import React from 'react'
+
+const projects = () => {
+  return (
+    <>
+    <div>projects</div>
+    <Carousel/></>
+  )
+}
+
+export default projects
